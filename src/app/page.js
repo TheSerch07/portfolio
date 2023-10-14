56af6b4c2b12f79d2fc093c4810f90b97178427f
TheSerch07 <jejog50@gmail.com>
2023-10-13 19:05:26 -0500

    add nextjs

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className={styles.title}>
+        Mi portafolio
+      </h1>
+    </main>
+  )
+}
